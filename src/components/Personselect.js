0db35d9fbbcdf286ec96af0dd57dc86555f556e7
Nirmal <technktailor@gmail.com>
2024-01-31 23:31:13 +0530

    Added components html

--- src/components/Personselect.js
+++ src/components/Personselect.js
@@ -3,13 +3,30 @@ import "./Personselect.css";
 export default function Personselect() {
   return (
     <div className="row" style={{ padding: "10px" }}>
-      <div className="col-sm-12">
+      <div className="col-12">
         <div class="card" style={{ width: "18rem" }}>
-          <div class="card-header">Featured</div>
+          <div class="card-header">Select Person to Invite</div>
           <ul class="list-group list-group-flush">
-            <li class="list-group-item">An item</li>
-            <li class="list-group-item">A second item</li>
-            <li class="list-group-item">A third item</li>
+            <li class="list-group-item">
+              Rick Martin
+              <span className="person-select-check">
+                <input
+                  class="form-check-input mt-1"
+                  type="checkbox"
+                  value="Person 1"
+                  aria-label="Checkbox for following text input"
+                  title="Select Rick Martin"
+                />
+              </span>
+            </li>
+            <li class="list-group-item">Abish Matthew</li>
+            <li class="list-group-item">Tim Bernett</li>
+            <li class="list-group-item">Abish Matthew</li>
+            <li class="list-group-item">Tim Bernett</li>
+            <li class="list-group-item">Abish Matthew</li>
+            <li class="list-group-item">Tim Bernett</li>
+            <li class="list-group-item">Abish Matthew</li>
+            <li class="list-group-item">Tim Bernett</li>
           </ul>
         </div>
       </div>
--- src/components/Sendinvite.js
+++ src/components/Sendinvite.js
@@ -1,27 +1,82 @@
 export default function Sendinvite() {
   return (
     <div className="row">
-      <div className="col-sm-6">
-        <div class="mb-3">
-          <label for="exampleFormControlInput1" class="form-label">
-            Email address
-          </label>
+      <div className="col-8">
+        <div class="input-group input-group-sm mb-3 flex-nowrap">
+          <span class="input-group-text" id="addon-wrapping">
+            From
+          </span>
           <input
-            type="email"
+            type="text"
+            readOnly
             class="form-control"
-            id="exampleFormControlInput1"
-            placeholder="name@example.com"
+            placeholder="meeting.tailorsthought@ymail.com"
+            aria-label="Username"
+            aria-describedby="addon-wrapping"
           />
         </div>
-        <div class="mb-3">
-          <label for="exampleFormControlTextarea1" class="form-label">
-            Example textarea
-          </label>
-          <textarea
+        <div class="input-group input-group-sm mb-3 flex-nowrap">
+          <span class="input-group-text" id="addon-wrapping">
+            Subject
+          </span>
+          <input
+            type="text"
             class="form-control"
-            id="exampleFormControlTextarea1"
-            rows="3"
+            placeholder="Meeting Purpose"
+            aria-label="figma"
+            aria-describedby="addon-wrapping"
+          />
+        </div>
+        <div class="input-group input-group-sm mb-3 flex-nowrap">
+          <span class="input-group-text" id="addon-wrapping">
+            To
+          </span>
+          <input
+            type="text"
+            class="form-control"
+            placeholder="Person 1, Person 2"
+            aria-label="figma"
+            aria-describedby="addon-wrapping"
+          />
+        </div>
+        <div class="input-group input-group-sm mb-3 flex-nowrap">
+          <span class="input-group-text" id="addon-wrapping">
+            Time
+          </span>
+          <input
+            type="text"
+            class="form-control"
+            placeholder="04:15 PM"
+            aria-label="figma"
+            aria-describedby="addon-wrapping"
+          />
+        </div>
+        <div class="input-group input-group-sm mb-3 flex-nowrap">
+          <span class="input-group-text" id="addon-wrapping">
+            Room
+          </span>
+
+          <select
+            class="form-select form-select-sm"
+            aria-label="Small select example"
+          >
+            <option selected>Select from Available room</option>
+            <option value="1">One</option>
+            <option value="2">Two</option>
+            <option value="3">Three</option>
+          </select>
+        </div>
+        <div class="input-group input-group-sm mb-3 flex-nowrap">
+          <textarea
+            rows={8}
+            className="form-control"
+            aria-label="with textarea"
           ></textarea>
+        </div>
+        <div>
+          <button type="button" class="btn btn-primary">
+            Send
+          </button>
         </div>
       </div>
     </div>
